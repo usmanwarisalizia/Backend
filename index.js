480891053fc1b9451cd34f7943e0b7c962494349
usman waris <usmanwaris1999@gmail.com>
2025-08-24 18:17:54 +0500

    github data added

--- index.js
+++ index.js
@@ -10,6 +10,42 @@ const express = require('express') // require module syntax
 const app = express() // synatx ko use karte huve ik variable banaya hai app ka
 const port = 3000 // port number virtual port number 
 
+const githubData = {
+    "login": "usmanwarisalizia",
+    "id": 113368971,
+    "node_id": "U_kgDOBsHfiw",
+    "avatar_url": "https://avatars.githubusercontent.com/u/113368971?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/usmanwarisalizia",
+    "html_url": "https://github.com/usmanwarisalizia",
+    "followers_url": "https://api.github.com/users/usmanwarisalizia/followers",
+    "following_url": "https://api.github.com/users/usmanwarisalizia/following{/other_user}",
+    "gists_url": "https://api.github.com/users/usmanwarisalizia/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/usmanwarisalizia/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/usmanwarisalizia/subscriptions",
+    "organizations_url": "https://api.github.com/users/usmanwarisalizia/orgs",
+    "repos_url": "https://api.github.com/users/usmanwarisalizia/repos",
+    "events_url": "https://api.github.com/users/usmanwarisalizia/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/usmanwarisalizia/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": null,
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 17,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2022-09-12T15:11:25Z",
+    "updated_at": "2025-08-24T12:22:02Z"
+}
+
 app.get('/', (req, res) => { // req = request, res = response // this is a call back functtion 
     res.send('Hello World!') // response send karta hai hello world
 })
@@ -25,6 +61,10 @@ app.get('/instagram', (req, res) => {
 
 app.get('/facebook', (req, res) => {
     res.send('<h1>this is facebook</h1>') // hotreloading
+})
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
