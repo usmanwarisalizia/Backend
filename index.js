2834be0b032c8988b8aabbbba3836e08e5d41585
usman waris <usmanwaris1999@gmail.com>
2025-08-24 18:26:37 +0500

    Add the express code

--- index.js
+++ index.js
@@ -72,3 +72,16 @@ app.listen(process.env.PORT, () => {
 })
 
 // .env
+
+
+// const express = require('express)
+
+// const app = express()
+// const port = 3000
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
